13489830191cddec5a29dff59cd1b0fba93d4571
lowcode-server <lowcode.server@lowcode.com>
2026-05-21 02:36:54 +0800

    对话: 修复主页按钮未读取 BrowserSettingsState 中设置的主页 URL

--- src/main/kotlin/com/cpw/browser/action/GoHomeAction.kt
+++ src/main/kotlin/com/cpw/browser/action/GoHomeAction.kt
@@ -2,7 +2,7 @@ package com.cpw.browser.action
 
 import com.cpw.browser.WebBrowserIcons
 import com.cpw.browser.browser.BrowserTabManager
-import com.intellij.ide.util.PropertiesComponent
+import com.cpw.browser.settings.BrowserSettingsState
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
@@ -13,12 +13,8 @@ class GoHomeAction(private val tabManager: BrowserTabManager) : AnAction("主页
     }
 
     override fun actionPerformed(e: AnActionEvent) {
-        val homeUrl = PropertiesComponent.getInstance().getValue(HOME_URL_KEY, DEFAULT_HOME_URL)
-        tabManager.activeTab?.navigate(homeUrl ?: DEFAULT_HOME_URL)
-    }
-
-    companion object {
-        const val HOME_URL_KEY = "webbrowser.home.url"
-        const val DEFAULT_HOME_URL = "about:blank"
+        val settings = BrowserSettingsState.getInstance()
+        val homeUrl = settings.homePageUrl.ifBlank { "about:blank" }
+        tabManager.activeTab?.navigate(homeUrl)
     }
 }
