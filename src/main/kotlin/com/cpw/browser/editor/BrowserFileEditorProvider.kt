017dd7c8968634c774c5751d6f1343f97c176987
lowcode-server <lowcode.server@lowcode.com>
2026-05-21 09:31:32 +0800

    修复 BrowserFileEditorProvider 缺少 DumbAware 接口导致插件启动报错
    HIDE_DEFAULT_EDITOR 策略要求 FileEditorProvider 实现 DumbAware 接口，
    否则关闭 IDEA 重新打开时抛出 PluginException。

--- src/main/kotlin/com/cpw/browser/editor/BrowserFileEditorProvider.kt
+++ src/main/kotlin/com/cpw/browser/editor/BrowserFileEditorProvider.kt
@@ -3,13 +3,14 @@ package com.cpw.browser.editor
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorPolicy
 import com.intellij.openapi.fileEditor.FileEditorProvider
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
 /**
  * 浏览器面板的 FileEditorProvider，支持在编辑区显示浏览器。
  */
-class BrowserFileEditorProvider : FileEditorProvider {
+class BrowserFileEditorProvider : FileEditorProvider, DumbAware {
 
     override fun accept(project: Project, file: VirtualFile): Boolean {
         return file.fileType == BrowserFileType
