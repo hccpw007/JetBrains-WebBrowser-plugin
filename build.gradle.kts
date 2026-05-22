5c37309704a2f65a0da16a7a4b90f436f8635a2b
lowcode-server <lowcode.server@lowcode.com>
2026-05-22 09:12:17 +0800

    build for 2025.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea("2026.1.2")
+        intellijIdea("2025.1")
         testFramework(TestFrameworkType.Platform)
 
         // Add plugin dependencies for compilation here:
